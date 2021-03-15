54f810d89387d06ff251eed904bff1823f0adc62
Alice (PodlubnyAY) <abuandrew@gmail.com>
2021-03-15 16:22:09 +0300

    code: double

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b, max;
+    double a, b, max;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
